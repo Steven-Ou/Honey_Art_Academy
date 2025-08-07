954b1f6d59ce87e4a7b0f2f3891f61c25fa1ff66
Steven-Ou <osteve425@gmail.com>
2025-08-06 22:47:03 -0400

    exporting the header()

--- honey-academy/components/Header.js
+++ honey-academy/components/Header.js
@@ -4,3 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faGraduationCap, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+export default function Header(){
+    
+}
