191e40f50669af5c9be2d4bff6a43ba1d06bb703
Steven-Ou <osteve425@gmail.com>
2025-08-06 22:46:07 -0400

    adding in imports

--- honey-academy/components/Header.js
+++ honey-academy/components/Header.js
@@ -3,3 +3,4 @@
 import { useState } from "react";
 import Link from "next/link";
 import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faGraduationCap, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
