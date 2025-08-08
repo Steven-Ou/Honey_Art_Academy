e9c86f22adf907e8164d4f08f8ffe53a450cfe48
Steven-Ou <osteve425@gmail.com>
2025-08-08 00:41:36 -0400

    decorating the div

--- honey-academy/components/Hero.js
+++ honey-academy/components/Hero.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Hero(){
     return(
         <section className="hero-bg text-white">
-            <div>
+      <     div className="container mx-auto px-6 py-32 text-center">
                 <h1></h1>
                 <p></p>
             </div>  
