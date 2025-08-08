7cc944062fab9cad4f76ed7329817ad4174f6508
Steven-Ou <osteve425@gmail.com>
2025-08-08 00:12:36 -0400

    creating a div

--- honey-academy/components/Header.js
+++ honey-academy/components/Header.js
@@ -32,7 +32,9 @@ export default function Header(){
         </div>
         {/* Mobile Menu */}
         {isOpen && (
-            
+            <div>
+                
+            </div>
         )}
 
     </header>
