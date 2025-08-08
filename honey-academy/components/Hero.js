864512ff545a35b19aa6abcbccbf3a81a175631a
Steven-Ou <osteve425@gmail.com>
2025-08-08 00:41:51 -0400

    adding in h1

--- honey-academy/components/Hero.js
+++ honey-academy/components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero(){
     return(
         <section className="hero-bg text-white">
       <     div className="container mx-auto px-6 py-32 text-center">
-                <h1></h1>
+        <   h1 className="text-4xl md:text-6xl font-bold leading-tight mb-4">Nurturing Tomorrow's Leaders & Artists</h1>
                 <p></p>
             </div>  
             <Link>
