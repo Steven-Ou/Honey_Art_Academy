4dd63ab89e551fd518c1892e5fb0e05a19fbc870
Steven-Ou <osteve425@gmail.com>
2025-08-08 00:43:06 -0400

    fixing the div

--- honey-academy/components/Hero.js
+++ honey-academy/components/Hero.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export default function Hero(){
     return(
         <section className="hero-bg text-white">
-      <     div className="container mx-auto px-6 py-32 text-center">
-        <   h1 className="text-4xl md:text-6xl font-bold leading-tight mb-4">Nurturing Tomorrow's Leaders & Artists</h1>
-                <p></p>
-            </div>  
+            <div className="container mx-auto px-6 py-32 text-center">
+                <h1 className="text-4xl md:text-6xl font-bold leading-tight mb-4">Nurturing Tomorrow's Leaders & Artists</h1>
+                <p className="text-lg md:text-xl mb-8 max-w-3xl mx-auto">Located in the heart of Flushing, Honey Academy offers a rich blend of academic and creative programs to help your child thrive.</p> 
             <Link>
                 Explore Our Programs
             </Link>
+            </div> 
         </section>
     )
 }
