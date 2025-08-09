1aa4c61a23608ee0a2104d019c9bce4eb19c95c5
Steven-Ou <osteve425@gmail.com>
2025-08-09 00:21:07 -0400

    adding in p

--- honey-academy/components/About.js
+++ honey-academy/components/About.js
@@ -20,7 +20,7 @@ export default function About(){
                                 <p></p>
                             </div>
                             <div>
-
+                                <p></p>
                             </div>
                             <div>
 
