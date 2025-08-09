00dd0abd1e2bfcd431096c77bf1690f5f5d44c77
Steven-Ou <osteve425@gmail.com>
2025-08-09 13:11:41 -0400

    adding in all the import

--- honey-academy/components/ProgramCard.js
+++ honey-academy/components/ProgramCard.js
@@ -1,1 +1,6 @@
 "use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
