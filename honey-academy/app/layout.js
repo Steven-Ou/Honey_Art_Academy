88ba9bbaacaeaef316a282fd9e2ca7f610367c6a
Steven-Ou <osteve425@gmail.com>
2025-08-09 13:09:55 -0400

    reformatting the file

--- honey-academy/app/layout.js
+++ honey-academy/app/layout.js
@@ -1,18 +1,19 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 // This config is needed for Font Awesome to work correctly with server components
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Honey Academy - Flushing After School, Dance, Art & Music',
-  description: "Honey Academy offers top-tier after-school, dance, art, and music programs in Flushing, NY. Nurturing tomorrow's leaders and artists.",
+  title: "Honey Academy - Flushing After School, Dance, Art & Music",
+  description:
+    "Honey Academy offers top-tier after-school, dance, art, and music programs in Flushing, NY. Nurturing tomorrow's leaders and artists.",
 };
 
 export default function RootLayout({ children }) {
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} bg-amber-50 text-gray-800`}>
         <Header />
         <main>{children}</main>
-       {/*<Footer/>*/} 
+        {/*<Footer/>*/}
       </body>
     </html>
   );
