5e581db79a2acfe54e3fa04ecc0f893fd44b8872
Steven-Ou <osteve425@gmail.com>
2025-08-09 13:32:18 -0400

    adding in the Footer function

---
+++ honey-academy/components/Footer.js
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <footer className="bg-amber-200">
+      <div className="container mx-auto px-6 py-8">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <p className="text-amber-800">&copy; {new Date().getFullYear()} Honey Academy. All Rights Reserved.</p>
+          <div className="flex space-x-4 mt-4 md:mt-0">
+            <Link href="#" className="text-amber-700 hover:text-amber-900 text-xl"><FontAwesomeIcon icon={faFacebookF} /></Link>
+            <Link href="#" className="text-amber-700 hover:text-amber-900 text-xl"><FontAwesomeIcon icon={faInstagram} /></Link>
+            <Link href="#" className="text-amber-700 hover:text-amber-900 text-xl"><FontAwesomeIcon icon={faYoutube} /></Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
