b9a07c9ec2404ac0d4bf127465d10074298c6cff
Steven-Ou <osteve425@gmail.com>
2025-08-09 00:51:23 -0400

    decorating the svg

--- honey-academy/components/Header.js
+++ honey-academy/components/Header.js
@@ -6,7 +6,7 @@ import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faGraduationCap, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Logo = () =>(
-    <svg>
+    <svg width="200" height="40" viewBox="0 0 200 40" fill="none" xmlns="http://www.w3.org/2000/svg">
         <text>
             Honey Academy
         </text>
