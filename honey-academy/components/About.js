1fb9eca00b0437f42166fa3644527a2790a475a4
Steven-Ou <osteve425@gmail.com>
2025-08-08 23:56:17 -0400

    adding in div and image

--- honey-academy/components/About.js
+++ honey-academy/components/About.js
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function About(){
     return(
         <section>
-            
+            <div>
+                <div>
+                    <Image
+                    
+                    />
+                </div>
+            </div>
         </section>
     )
 }
