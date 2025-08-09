e21beb10fe1f766528419cddb0534db5a7eab638
Steven-Ou <osteve425@gmail.com>
2025-08-09 00:50:29 -0400

    creating svg and text

--- honey-academy/components/Header.js
+++ honey-academy/components/Header.js
@@ -6,7 +6,11 @@ import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faGraduationCap, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Logo = () =>(
-
+    <svg>
+        <text>
+            
+        </text>
+    </svg>
 );
 export default function Header(){
   const [isOpen ,setIsOpen] = useState(false);
