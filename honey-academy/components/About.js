8e1394730d0887e0a8279f87ece4d15b31eb4ee9
Steven-Ou <osteve425@gmail.com>
2025-08-09 00:18:20 -0400

    adding in div

--- honey-academy/components/About.js
+++ honey-academy/components/About.js
@@ -16,6 +16,9 @@ export default function About(){
                     <div>
                         <div>
                             <div>
+
+                            </div>
+                            <div>
                                 
                             </div>
                         </div>
