a415bad776af5b38f2b6ede943217d768c4361d7
Steven-Ou <osteve425@gmail.com>
2025-08-10 15:02:12 -0400

    Updating the next,config.mjs

--- honey-academy/next.config.mjs
+++ honey-academy/next.config.mjs
@@ -1,12 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
