f4014baa58f8eda87c4733db78fccf9d837557c0
Steven-Ou <osteve425@gmail.com>
2025-08-10 15:12:05 -0400

    Fixing the structure

--- honey-academy/app/layout.js
+++ honey-academy/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 // This config is needed for Font Awesome to work correctly with server components
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -27,15 +27,21 @@ export default function RootLayout({ children }) {
     }
 
     if (savedBg) {
-      document.documentElement.style.setProperty("--background-image", savedBg === "none" ? "none" : `url(${savedBg})`);
+      document.documentElement.style.setProperty(
+        "--background-image",
+        savedBg === "none" ? "none" : `url(${savedBg})`
+      );
     }
   }, []);
-  
+
   return (
     <html lang="en" className="scroll-smooth">
       <head>
         <title>Honey Academy - Flushing After School, Dance, Art & Music</title>
-        <meta name="description" content="Honey Academy offers top-tier after-school, dance, art, and music programs in Flushing, NY. Nurturing tomorrow's leaders and artists." />
+        <meta
+          name="description"
+          content="Honey Academy offers top-tier after-school, dance, art, and music programs in Flushing, NY. Nurturing tomorrow's leaders and artists."
+        />
       </head>
       <body className={`${inter.className} bg-base text-gray-800`}>
         <Header />
