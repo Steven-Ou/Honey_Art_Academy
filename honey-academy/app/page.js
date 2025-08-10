96cc16a4d3a8725a0234cc3b17c6690c276df512
Steven-Ou <osteve425@gmail.com>
2025-08-10 15:32:48 -0400

    fetching this data and pass it to the components

--- honey-academy/app/page.js
+++ honey-academy/app/page.js
@@ -3,15 +3,19 @@ import About from "../components/About";
 import Programs from "../components/Programs";
 import Testimonials from "../components/Testimonials";
 import Contact from "../components/Contact";
+import { getPrograms, getTestimonials } from "../lib/cms";
 
-export default function Home() {
+export default async function Home() {
+  const programs = await getPrograms();
+  const testimonials = await getTestimonials();
+
   return (
     <>
       <Hero />
       <div className="container mx-auto px-6 py-16">
         <About />
-        <Programs />
-        <Testimonials />
+        <Programs programs={programs} />
+        <Testimonials testimonials={testimonials} />
         <Contact />
       </div>
     </>
