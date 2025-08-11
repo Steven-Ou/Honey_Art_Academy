afab35426680de6620f86fbaf755db65aacdceb5
Steven-Ou <osteve425@gmail.com>
2025-08-11 12:39:56 -0400

    css updated

--- honey-academy/components/ProgramCard.js
+++ honey-academy/components/ProgramCard.js
@@ -1,5 +1,4 @@
 "use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,7 +6,6 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function ProgramCard({ imgSrc, title, description, link }) {
   return (
-    // Removed fixed width to allow the card to be responsive within the grid
     <div className="bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform duration-300 flex flex-col group">
       <div className="relative h-48 w-full">
         <Image
@@ -20,7 +18,7 @@ export default function ProgramCard({ imgSrc, title, description, link }) {
       </div>
       <div className="p-6 flex-grow flex flex-col">
         <h3 className="text-xl font-bold mb-2 text-primary-dark">{title}</h3>
-        <p className="text-gray-600 mb-4 flex-grow">{description}</p>
+        <p className="text-text-muted mb-4 flex-grow">{description}</p>
         <Link
           href={link}
           className="font-semibold text-primary hover:text-primary-dark mt-auto self-start"
