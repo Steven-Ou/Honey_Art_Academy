4cf7c60d0e98859793c3caf280ae680c9f740403
Steven-Ou <osteve425@gmail.com>
2025-08-11 12:44:30 -0400

    adding in tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#fbebeb',
+          DEFAULT: '#a32a2a',
+          dark: '#822121',
+        },
+        secondary: '#f3f4f6',
+        text: {
+          base: '#1f2937',
+          muted: '#6b7280',
+        }
+      },
+      fontFamily: {
+        inter: ['var(--font-inter)', 'sans-serif'],
+        lora: ['var(--font-lora)', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
