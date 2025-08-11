9e46e5b130820cce3f5ba33eca6fbb15149ed1bf
Steven-Ou <osteve425@gmail.com>
2025-08-11 12:58:18 -0400

    updating the postcssconfig.mjs

--- honey-academy/postcss.config.mjs
+++ honey-academy/postcss.config.mjs
@@ -1,5 +1,8 @@
+import tailwind from 'tailwindcss';
+import tailwindConfig from './tailwind.config.mjs';
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [tailwind(tailwindConfig)],
 };
 
 export default config;
