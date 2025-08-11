a978ef5b0015f80212b594437371049d3bc4f4c9
Steven-Ou <osteve425@gmail.com>
2025-08-11 12:50:48 -0400

    Fixing and adding

--- honey-academy/tailwind.config.mjs
+++ honey-academy/tailwind.config.mjs
@@ -1,28 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+const config = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: {
-          light: '#fbebeb',
-          DEFAULT: '#a32a2a',
-          dark: '#822121',
+          light: "#fbebeb",
+          DEFAULT: "#a32a2a",
+          dark: "#822121",
         },
-        secondary: '#f3f4f6',
+        secondary: "#f3f4f6",
         text: {
-          base: '#1f2937',
-          muted: '#6b7280',
-        }
+          base: "#1f2937",
+          muted: "#6b7280",
+        },
       },
       fontFamily: {
-        inter: ['var(--font-inter)', 'sans-serif'],
-        lora: ['var(--font-lora)', 'serif'],
+        inter: ["var(--font-inter)", "sans-serif"],
+        lora: ["var(--font-lora)", "serif"],
       },
     },
   },
   plugins: [],
 };
+export default config;
