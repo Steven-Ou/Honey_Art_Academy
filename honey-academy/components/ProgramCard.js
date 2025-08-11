5b62971d58ff811f687b7826b5f46bf0f6bdb8cb
Steven-Ou <osteve425@gmail.com>
2025-08-11 12:51:04 -0400

    Updating

--- honey-academy/components/ProgramCard.js
+++ honey-academy/components/ProgramCard.js
@@ -11,7 +11,8 @@ export default function ProgramCard({ imgSrc, title, description, link }) {
         <Image
           src={imgSrc}
           alt={`${title} Program`}
-          layout="fill"
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           className="object-cover group-hover:scale-105 transition-transform duration-300"
           unoptimized={true}
         />
