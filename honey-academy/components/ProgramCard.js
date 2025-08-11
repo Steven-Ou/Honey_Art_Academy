5d0bd4ccb2f0061192244d8bfd6d092ad900af9e
Steven-Ou <osteve425@gmail.com>
2025-08-11 13:15:15 -0400

    improving the loading performance

--- honey-academy/components/ProgramCard.js
+++ honey-academy/components/ProgramCard.js
@@ -1,3 +1,5 @@
+// steven-ou/honey_art_academy/Honey_Art_Academy-b30d0db627a43b328427977d4914901b7229c657/honey-academy/components/ProgramCard.js
+
 "use client";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,7 +8,7 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function ProgramCard({ imgSrc, title, description, link }) {
   return (
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform duration-300 flex flex-col group">
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 hover:shadow-2xl transition-all duration-300 flex flex-col group">
       <div className="relative h-48 w-full">
         <Image
           src={imgSrc}
@@ -14,7 +16,6 @@ export default function ProgramCard({ imgSrc, title, description, link }) {
           fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           className="object-cover group-hover:scale-105 transition-transform duration-300"
-          unoptimized={true}
         />
       </div>
       <div className="p-6 flex-grow flex flex-col">
@@ -22,9 +23,13 @@ export default function ProgramCard({ imgSrc, title, description, link }) {
         <p className="text-text-muted mb-4 flex-grow">{description}</p>
         <Link
           href={link}
-          className="font-semibold text-primary hover:text-primary-dark mt-auto self-start"
+          className="font-semibold text-primary hover:text-primary-dark mt-auto self-start group"
         >
-          Read More <FontAwesomeIcon icon={faArrowRight} className="ml-1" />
+          Read More{" "}
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            className="ml-1 group-hover:translate-x-1 transition-transform"
+          />
         </Link>
       </div>
     </div>
