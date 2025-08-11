acc0072ab46c66ded4ebe30957e34d4e71ee8fe2
Steven-Ou <osteve425@gmail.com>
2025-08-11 14:11:06 -0400

    updating

--- honey-academy/app/page.js
+++ honey-academy/app/page.js
@@ -1,25 +1,17 @@
-// steven-ou/honey_art_academy/Honey_Art_Academy-3b08cfc3f42b337480a330f9b0097aaca9ef5160/honey-academy/app/page.js
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Programs from "@/components/Programs";
+import Testimonials from "@/components/Testimonials";
+import Contact from "@/components/Contact";
 
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Programs from "../components/Programs";
-import Testimonials from "../components/Testimonials";
-import Contact from "../components/Contact";
-import { getPrograms, getTestimonials } from "../lib/cms";
-
-export default async function Home() {
-  const programs = await getPrograms();
-  const testimonials = await getTestimonials();
-
+export default function Home() {
   return (
-    <>
+    <main>
       <Hero />
-      <div className="container mx-auto px-6">
-        <About />
-        <Programs programs={programs} />
-      </div>
-      <Testimonials testimonials={testimonials} />
+      <About />
+      <Programs />
+      <Testimonials />
       <Contact />
-    </>
+    </main>
   );
 }
