4d31e1fa8150944e2b1f8f9fe24ce6be1173754d
Steven-Ou <osteve425@gmail.com>
2025-08-12 13:24:46 -0400

    adding in new blueprint

--- honey-academy/sanity/schemaTypes/index.ts
+++ honey-academy/sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { program } from "./program";
 import { testimonial } from "./testimonial";
+import { galleryItem } from "./galleryItem";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [program, testimonial],
+  types: [program, testimonial, galleryItem],
 };
