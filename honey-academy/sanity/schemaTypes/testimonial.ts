b25c4731be3afe82f36736b6f56b3d1df43d2b64
Steven-Ou <osteve425@gmail.com>
2025-08-12 01:21:31 -0400

    Adding in the following testimonial code

---
+++ honey-academy/sanity/schemaTypes/testimonial.ts
@@ -0,0 +1,21 @@
+import {defineField, defineType} from 'sanity'
+
+export const testimonial = defineType({
+  name: 'testimonial',
+  title: 'Testimonial',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'quote',
+      title: 'Quote',
+      type: 'text',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+  ],
+})
