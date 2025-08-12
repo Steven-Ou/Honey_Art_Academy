77bc0dc6661d49f6d2139b3a18843dfe1c1d1e00
Steven-Ou <osteve425@gmail.com>
2025-08-12 01:20:34 -0400

    adding in the types array

--- honey-academy/sanity/schemaTypes/index.ts
+++ honey-academy/sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { program } from './program'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [program],
 }
