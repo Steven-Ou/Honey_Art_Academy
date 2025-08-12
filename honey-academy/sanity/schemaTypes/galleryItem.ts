de8673d5b71494d30160780d2b6abbeba81bffb1
Steven-Ou <osteve425@gmail.com>
2025-08-12 13:47:40 -0400

    Updating the blue prints

--- honey-academy/sanity/schemaTypes/galleryItem.ts
+++ honey-academy/sanity/schemaTypes/galleryItem.ts
@@ -17,7 +17,21 @@ export const galleryItem = defineType({
       name: 'details',
       title: 'Details',
       type: 'array',
-      of: [{type: 'block'}],
+      of: [{type: 'block'}], // You can create subheadings here in the Studio
+    }),
+    // Add a field for the video URL
+    defineField({
+      name: 'videoUrl',
+      title: 'Video URL',
+      description: 'Paste the full URL of a YouTube or Vimeo video.',
+      type: 'url',
+    }),
+    // Add a field for the contact button link
+    defineField({
+      name: 'contactUrl',
+      title: 'Contact Button URL',
+      description: 'The URL the contact button should link to (e.g., /#contact).',
+      type: 'string',
     }),
   ],
 })
