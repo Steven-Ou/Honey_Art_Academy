b5aec2fd4371b782aec1c5722a9c8b64fee42be9
Steven-Ou <osteve425@gmail.com>
2025-08-13 02:19:46 -0400

    updating

--- honey-academy/sanity.config.ts
+++ honey-academy/sanity.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'sanity';
+// As the deprecation message advises, we use 'structureTool' from 'sanity/structure'
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 
-// Import your schema and the custom structure
+// Import your schema and custom structure
 import { schema } from './sanity/schemaTypes';
-import { structure as customStructure } from './sanity/structure'; // Renaming the import here
+// Rename your imported structure to avoid name conflicts
+import { structure as customStructure } from './sanity/structure'; 
 
 export default defineConfig({
   basePath: '/studio',
@@ -14,8 +16,8 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [
+    // Use the modern 'structureTool'
     structureTool({
-      // Use the renamed custom structure here
       structure: customStructure, 
     }),
     visionTool(),
