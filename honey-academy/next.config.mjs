d0bbf44cc80b7a71dc901462b17d3cc60358da82
Steven-Ou <osteve425@gmail.com>
2025-08-13 02:30:03 -0400

    updating

--- honey-academy/next.config.mjs
+++ honey-academy/next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add this experimental block to fix the error
   experimental: {
+    // This tells Next.js to re-compile these packages
+    // in a way that follows its new, stricter rules.
     transpilePackages: ["sanity", "framer-motion"],
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
-      },
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
