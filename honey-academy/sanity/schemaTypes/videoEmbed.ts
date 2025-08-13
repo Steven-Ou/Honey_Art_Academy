46bac0486f799a63c199ed1749c29e6282946bad
Steven-Ou <osteve425@gmail.com>
2025-08-13 14:52:10 -0400

    updating

--- honey-academy/sanity/schemaTypes/videoEmbed.ts
+++ honey-academy/sanity/schemaTypes/videoEmbed.ts
@@ -1,14 +1,28 @@
-import { defineType, defineField } from "sanity";
+import {defineType, defineField} from 'sanity'
+import {PlayIcon} from '@sanity/icons'
 
 export const videoEmbed = defineType({
-  name: "videoEmbed",
-  title: "Video Embed",
-  type: "object",
+  name: 'videoEmbed',
+  title: 'Video Embed',
+  type: 'object',
+  icon: PlayIcon, // Adds a nice icon
   fields: [
     defineField({
-      name: "url",
-      type: "url",
-      title: "YouTube or Vimeo URL",
+      name: 'url',
+      type: 'url',
+      title: 'YouTube or Vimeo URL',
     }),
   ],
-});
+  // This part customizes the title you see in the list
+  preview: {
+    select: {
+      url: 'url',
+    },
+    prepare({url}) {
+      return {
+        title: 'Video Embed',
+        subtitle: url || '(No URL set)',
+      }
+    },
+  },
+})
