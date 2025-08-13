2e8541b396ff103edb8b8fe779c4b92f35d3bca9
Steven-Ou <osteve425@gmail.com>
2025-08-13 14:25:46 -0400

    adding in a new type

--- honey-academy/sanity/schemaTypes/index.ts
+++ honey-academy/sanity/schemaTypes/index.ts
@@ -3,7 +3,8 @@ import { program } from "./program";
 import { testimonial } from "./testimonial";
 import { galleryItem } from "./galleryItem";
 import { aboutPage } from "./aboutPage";
+import { videoEmbed } from "./videoEmbed";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [program, testimonial, galleryItem, aboutPage],
+  types: [program, testimonial, galleryItem, aboutPage,videoEmbed],
 };
