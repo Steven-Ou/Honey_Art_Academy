fd639ab1649f8f2791e337a429bf46b9808629b8
Steven-Ou <osteve425@gmail.com>
2025-08-13 01:16:46 -0400

    adding in the aboutpage blueprint

---
+++ honey-academy/sanity/schemaTypes/aboutPage.ts
@@ -0,0 +1,22 @@
+import {defineField, defineType} from 'sanity'
+
+export const aboutPage = defineType({
+  name: 'aboutPage',
+  title: 'About Page',
+  type: 'document',
+  fields: [
+    defineField({name: 'title', title: 'Title', type: 'string'}),
+    defineField({name: 'heroImage', title: 'Hero Image', type: 'image'}),
+    defineField({name: 'storyTitle', title: 'Story Title', type: 'string'}),
+    defineField({name: 'storyText', title: 'Story Text', type: 'array', of: [{type: 'block'}]}),
+    defineField({name: 'storyImage', title: 'Story Image', type: 'image'}),
+    defineField({name: 'philosophyTitle', title: 'Philosophy Title', type: 'string'}),
+    defineField({name: 'philosophyText', title: 'Philosophy Text', type: 'array', of: [{type: 'block'}]}),
+    defineField({name: 'philosophyImage', title: 'Philosophy Image', type: 'image'}),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+})
