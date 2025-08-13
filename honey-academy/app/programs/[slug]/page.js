ba38c4efb91c2fbc8631d6642e0c484a56b05f69
Steven-Ou <osteve425@gmail.com>
2025-08-13 01:53:02 -0400

    updating

--- honey-academy/app/programs/[slug]/page.js
+++ honey-academy/app/programs/[slug]/page.js
@@ -54,7 +54,7 @@ export default async function ProgramPage({ params }) {
             <h2 className="text-3xl font-bold text-secondary mb-6">
               Explore Our Instruments
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-2 gap-8">
               {program.gallery?.filter(Boolean).map((item) => (
                 <Link key={item._id} href={`/gallery/${item.slug.current}`}>
                   {/* New Card Design */}
