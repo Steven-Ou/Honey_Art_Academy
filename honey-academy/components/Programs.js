01909868eaa9e4e4c7590524ead76f3fb6a64095
Steven-Ou <osteve425@gmail.com>
2025-08-13 01:05:02 -0400

    updating

--- honey-academy/components/Programs.js
+++ honey-academy/components/Programs.js
@@ -49,7 +49,7 @@ export default async function Programs() {
             programs to ignite your child's passion.
           </p>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-2  gap-8">
           {programData.map((prog) => (
             <ProgramCard
               key={prog._id}
