12816ef8aa7df1ee29c96fe0dc43a5e6141706a0
Steven-Ou <osteve425@gmail.com>
2025-08-13 01:17:04 -0400

    updating

--- honey-academy/sanity/schemaTypes/index.ts
+++ honey-academy/sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 import { program } from "./program";
 import { testimonial } from "./testimonial";
 import { galleryItem } from "./galleryItem";
+import { aboutPage } from "./aboutPage";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [program, testimonial, galleryItem],
+  types: [program, testimonial, galleryItem, aboutPage],
 };
