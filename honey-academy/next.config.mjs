626a2926668966aaaaef9bca8b97d2abbd967b0d
Steven-Ou <osteve425@gmail.com>
2025-08-13 11:28:48 -0400

    Upgrading to make it work?

--- honey-academy/next.config.mjs
+++ honey-academy/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // This tells Next.js to re-compile these packages
-    // in a way that follows its new, stricter rules.
-    transpilePackages: ["sanity", "framer-motion"],
-    esmExternals: "loose",
+    serverComponentsExternalPackages: ["@sanity/client"],
   },
   images: {
     remotePatterns: [
