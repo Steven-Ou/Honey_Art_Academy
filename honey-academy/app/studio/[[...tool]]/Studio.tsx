2a847f46e15c9a6aa990327488cf3488ffd80ef4
Steven-Ou <osteve425@gmail.com>
2025-08-13 11:52:30 -0400

    Updating

--- honey-academy/app/studio/[[...tool]]/Studio.tsx
+++ honey-academy/app/studio/[[...tool]]/Studio.tsx
@@ -4,6 +4,5 @@ import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity.config";
 
 export function Studio() {
-  //  Supports the same props as `import {Studio} from 'sanity'`
   return <NextStudio config={config} />;
 }
