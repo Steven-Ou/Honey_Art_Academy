dbafcdc432d4bfcc60b569447a1c415eada00a05
Steven-Ou <osteve425@gmail.com>
2025-08-13 02:12:01 -0400

    Updating

--- honey-academy/sanity.config.ts
+++ honey-academy/sanity.config.ts
@@ -1,28 +1,25 @@
-'use client'
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { visionTool } from '@sanity/vision';
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
- */
-
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
+// Import your schema and the custom structure
+import { schema } from './sanity/schemaTypes';
+import { structure as customStructure } from './sanity/structure'; // Renaming the import here
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
+  name: 'honey_art_academy',
+  title: 'Honey Art Academy',
+  projectId: 'mht1q7jc',
+  dataset: 'production',
+
   plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    structureTool({
+      // Use the renamed custom structure here
+      structure: customStructure, 
+    }),
+    visionTool(),
   ],
-})
+
+  schema,
+});
