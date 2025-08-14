5f03d6caaba088e8fa647c2544410a6ef9b2a93f
Steven-Ou <osteve425@gmail.com>
2025-08-14 02:18:28 -0400

    Updating the structure

--- honey-academy/sanity/structure.ts
+++ honey-academy/sanity/structure.ts
@@ -1,12 +1,13 @@
 export const structure = (S) =>
   S.list()
-    .title('Content')
+    .title("Content")
     .items([
       S.listItem()
-        .title('About Page')
-        .id('aboutPage')
-        .child(S.document().schemaType('aboutPage').documentId('aboutPage')),
+        .title("About Page")
+        .id("aboutPage")
+        .child(S.document().schemaType("aboutPage").documentId("aboutPage")),
+      S.documentTypeListItem("instructor").title("Instructors"),
       ...S.documentTypeListItems().filter(
-        (listItem) => !['aboutPage'].includes(listItem.getId())
+        (listItem) => !["aboutPage"].includes(listItem.getId())
       ),
-    ])
+    ]);
