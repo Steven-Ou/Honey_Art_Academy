da2655548e23035876a21ac2ce335ca26769f6dc
Steven-Ou <osteve425@gmail.com>
2025-08-14 10:49:15 -0400

    Adding in event.ts to

---
+++ honey-academy/sanity/schemaTypes/event.ts
@@ -0,0 +1,33 @@
+import { defineField, defineType } from "sanity";
+import { CalendarIcon } from "@sanity/icons";
+
+export const event = defineType({
+  name: "event",
+  title: "Event",
+  type: "document",
+  icon: CalendarIcon,
+  fields: [
+    defineField({
+      name: "title",
+      title: "Event Title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
+    }),
+    defineField({
+      name: "date",
+      title: "Event Date",
+      type: "datetime",
+    }),
+    // You can add a rich text editor for event details here if you like
+    // defineField({
+    //   name: 'details',
+    //   title: 'Event Details',
+    //   type: 'blockContent',
+    // }),
+  ],
+});
