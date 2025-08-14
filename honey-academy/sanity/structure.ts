acb99b3246b35fcb21de8788b949fd7f79affd55
Steven-Ou <osteve425@gmail.com>
2025-08-14 10:53:17 -0400

    adding in the newly added types into the structure

--- honey-academy/sanity/structure.ts
+++ honey-academy/sanity/structure.ts
@@ -2,13 +2,23 @@ export const structure = (S) =>
   S.list()
     .title("Content")
     .items([
+      // Add this new list item at the top
+      S.listItem()
+        .title("Site Settings")
+        .id("settings")
+        .icon(CogIcon)
+        .child(S.document().schemaType("settings").documentId("settings")),
+      S.divider(), // Adds a visual separator
       S.listItem()
         .title("About Page")
         .id("aboutPage")
         .child(S.document().schemaType("aboutPage").documentId("aboutPage")),
       S.documentTypeListItem("instructor").title("Instructors"),
-      S.documentTypeListItem("event").title("Events"),
+      S.documentTypeListItem("event").title("Events"), // Add a link to manage events
       ...S.documentTypeListItems().filter(
-        (listItem) => !["aboutPage", "instructor"].includes(listItem.getId())
+        (listItem) =>
+          !["aboutPage", "instructor", "settings", "event"].includes(
+            listItem.getId()
+          )
       ),
     ]);
