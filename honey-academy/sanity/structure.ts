907139f19c7be0613da66e56ac305f534c66ea91
Steven-Ou <osteve425@gmail.com>
2025-08-14 11:06:48 -0400

    Adding in cogIcon so it will work

--- honey-academy/sanity/structure.ts
+++ honey-academy/sanity/structure.ts
@@ -1,20 +1,21 @@
+import { CogIcon } from "@sanity/icons"; // Make sure this import is at the top
+
 export const structure = (S) =>
   S.list()
     .title("Content")
     .items([
-      // Add this new list item at the top
       S.listItem()
         .title("Site Settings")
         .id("settings")
-        .icon(CogIcon)
+        .icon(CogIcon) // This will now work
         .child(S.document().schemaType("settings").documentId("settings")),
-      S.divider(), // Adds a visual separator
+      S.divider(),
       S.listItem()
         .title("About Page")
         .id("aboutPage")
         .child(S.document().schemaType("aboutPage").documentId("aboutPage")),
       S.documentTypeListItem("instructor").title("Instructors"),
-      S.documentTypeListItem("event").title("Events"), // Add a link to manage events
+      S.documentTypeListItem("event").title("Events"),
       ...S.documentTypeListItems().filter(
         (listItem) =>
           !["aboutPage", "instructor", "settings", "event"].includes(
