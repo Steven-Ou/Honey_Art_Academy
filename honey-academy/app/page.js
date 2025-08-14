fe48be288c6dd0ecca5d169fa4dd8ab4651346a7
Steven-Ou <osteve425@gmail.com>
2025-08-14 12:30:28 -0400

    Updating

--- honey-academy/app/page.js
+++ honey-academy/app/page.js
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Programs from "@/components/Programs";
-import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
 
 export default function Home() {
