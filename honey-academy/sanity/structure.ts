9abdd51d9347b06c4941ac61382a6213006ea86b
Steven-Ou <osteve425@gmail.com>
2025-08-14 12:30:47 -0400

    FIxing the bug

--- honey-academy/sanity/structure.ts
+++ honey-academy/sanity/structure.ts
@@ -1,5 +1,5 @@
-import { CogIcon } from "@sanity/icons"; // Make sure this import is at the top
-import {CameraIcon} from "@sanity/icons";
+import { CogIcon, UserIcon, ImagesIcon } from "@sanity/icons"; // Change CameraIcon to ImagesIcon
+
 export const structure = (S) =>
   S.list()
     .title("Content")
@@ -7,26 +7,31 @@ export const structure = (S) =>
       S.listItem()
         .title("Site Settings")
         .id("settings")
-        .icon(CogIcon) // This will now work
+        .icon(CogIcon)
         .child(S.document().schemaType("settings").documentId("settings")),
       S.divider(),
       S.listItem()
         .title("Facilities Page")
         .id("facilitiesPage")
-        .icon(CameraIcon)
+        .icon(ImagesIcon) // Use the correct icon here
         .child(
           S.document().schemaType("facilitiesPage").documentId("facilitiesPage")
         ),
       S.listItem()
         .title("About Page")
         .id("aboutPage")
+        .icon(UserIcon)
         .child(S.document().schemaType("aboutPage").documentId("aboutPage")),
       S.documentTypeListItem("instructor").title("Instructors"),
       S.documentTypeListItem("event").title("Events"),
       ...S.documentTypeListItems().filter(
         (listItem) =>
-          !["aboutPage", "instructor", "settings", "event", "facilitiesPage"].includes(
-            listItem.getId()
-          )
+          ![
+            "aboutPage",
+            "instructor",
+            "settings",
+            "event",
+            "facilitiesPage",
+          ].includes(listItem.getId())
       ),
     ]);
