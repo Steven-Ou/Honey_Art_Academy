15efe14493f985ea6fec07d5593a4a358ed57231
Steven-Ou <osteve425@gmail.com>
2025-08-14 12:32:17 -0400

    Updating about page.js

--- honey-academy/app/about/page.js
+++ honey-academy/app/about/page.js
@@ -1,8 +1,7 @@
 import { client } from "@/sanity/lib/client";
-import AboutPageClient from "./AboutPageClient"; // We will keep using the client component for interactivity
+import AboutPageClient from "./AboutPageClient"; // Import our client component
 
 // This is the data-fetching function that was missing.
-// It runs on the server to get the content from Sanity.
 async function getAboutPage() {
   const query = `*[_type == "aboutPage"][0]{
     title,
@@ -28,7 +27,6 @@ async function getAboutPage() {
 export default async function AboutPage() {
   const data = await getAboutPage();
 
-  // If no data is found (e.g., the page isn't published), show a helpful message.
   if (!data) {
     return (
       <div className="text-center py-24">
@@ -41,6 +39,6 @@ export default async function AboutPage() {
     );
   }
 
-  // Pass the fetched data as a prop to our client component
+  // We pass the fetched data as a prop to our client component
   return <AboutPageClient pageData={data} />;
 }
