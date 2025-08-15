83d7213ec2d0e52d232d2232dbe21738deea8932
Steven-Ou <osteve425@gmail.com>
2025-08-15 11:47:02 -0400

    Adding in home page section, so the user can add in their own photo

---
+++ honey-academy/sanity/schemaTypes/homepage.ts
@@ -0,0 +1,28 @@
+import { defineField, defineType } from "sanity";
+import { HomeIcon } from "@sanity/icons";
+
+export const homePage = defineType({
+  name: "homePage",
+  title: "Homepage",
+  type: "document",
+  icon: HomeIcon,
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      initialValue: "Homepage",
+      readOnly: true,
+    }),
+    defineField({
+      name: "hero",
+      title: "Hero Section",
+      type: "heroSection",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+    },
+  },
+});
