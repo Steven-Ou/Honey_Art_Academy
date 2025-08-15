84c42cfa6841e35f4ae9dbd50bfa48a822f3d853
Steven-Ou <osteve425@gmail.com>
2025-08-15 12:16:31 -0400

    Updating

--- honey-academy/sanity/structure.ts
+++ honey-academy/sanity/structure.ts
@@ -1,21 +1,27 @@
-import { CogIcon, UserIcon, ImagesIcon, HomeIcon } from "@sanity/icons";
+import {
+  CogIcon,
+  UserIcon,
+  ImagesIcon,
+  HomeIcon,
+  CalendarIcon,
+  BookIcon,
+} from "@sanity/icons";
 
 export const structure = (S) =>
   S.list()
     .title("Content")
     .items([
-      // 1. Add the new Homepage singleton here
+      // Page Documents
       S.listItem()
         .title("Homepage")
         .id("homePage")
         .icon(HomeIcon)
         .child(S.document().schemaType("homePage").documentId("homePage")),
       S.listItem()
-        .title("Site Settings")
-        .id("settings")
-        .icon(CogIcon)
-        .child(S.document().schemaType("settings").documentId("settings")),
-      S.divider(),
+        .title("About Page")
+        .id("aboutPage")
+        .icon(UserIcon)
+        .child(S.document().schemaType("aboutPage").documentId("aboutPage")),
       S.listItem()
         .title("Facilities Page")
         .id("facilitiesPage")
@@ -23,22 +29,36 @@ export const structure = (S) =>
         .child(
           S.document().schemaType("facilitiesPage").documentId("facilitiesPage")
         ),
+
+      S.divider(),
+
+      // Content Types
+      S.documentTypeListItem("program").title("Programs").icon(BookIcon),
+      S.documentTypeListItem("instructor").title("Instructors").icon(UserIcon),
+      S.documentTypeListItem("event").title("Events").icon(CalendarIcon),
+      S.documentTypeListItem("galleryItem").title("Gallery Items"),
+
+      S.divider(),
+
+      // Settings
       S.listItem()
-        .title("About Page")
-        .id("aboutPage")
-        .icon(UserIcon)
-        .child(S.document().schemaType("aboutPage").documentId("aboutPage")),
-      S.documentTypeListItem("instructor").title("Instructors"),
-      S.documentTypeListItem("event").title("Events"),
+        .title("Site Settings")
+        .id("settings")
+        .icon(CogIcon)
+        .child(S.document().schemaType("settings").documentId("settings")),
+
+      // Filter out all the manually handled items from the default list
       ...S.documentTypeListItems().filter(
         (listItem) =>
           ![
+            "homePage",
             "aboutPage",
-            "instructor",
-            "settings",
-            "event",
             "facilitiesPage",
-            "homePage", // 2. Add "homePage" to this list to exclude it
+            "program",
+            "instructor",
+            "event",
+            "galleryItem",
+            "settings",
           ].includes(listItem.getId())
       ),
     ]);
