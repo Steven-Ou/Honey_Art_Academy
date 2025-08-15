20eb4771c4ae49f9be2d0aa19834225d8ef1e03b
Steven-Ou <osteve425@gmail.com>
2025-08-14 23:41:02 -0400

    Adding in the getSetting function

--- honey-academy/app/page.js
+++ honey-academy/app/page.js
@@ -2,14 +2,26 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Programs from "@/components/Programs";
 import Contact from "@/components/Contact";
+import { client } from "@/sanity/lib/client";
 
-export default function Home() {
+// Fetch settings data needed for this page
+async function getSettings() {
+  const query = `*[_type == "settings"][0]{
+    googleMapsEmbedUrl
+  }`;
+  return client.fetch(query);
+}
+
+export default async function Home() {
+  const settings = await getSettings();
+
   return (
     <main>
       <Hero />
       <About />
       <Programs />
-      <Contact />
+      {/* Pass the settings data to the Contact component */}
+      <Contact settings={settings} />
     </main>
   );
 }
