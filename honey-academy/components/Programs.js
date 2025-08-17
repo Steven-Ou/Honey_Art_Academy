1bb9c58b0f34ed4268591a6383a0c8e3df8aa6be
Steven-Ou <osteve425@gmail.com>
2025-08-17 02:22:49 -0400

    Updating the program cards decoration

--- honey-academy/components/Programs.js
+++ honey-academy/components/Programs.js
@@ -43,7 +43,7 @@ export default function Programs({ programsData, programCards }) {
             </p>
           )}
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {/* Use the programCards passed in via props */}
           {programCards?.map((prog) => (
             <ProgramCard
