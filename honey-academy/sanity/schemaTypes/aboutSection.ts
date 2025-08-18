5d6039d60cf58cf435ab5f0f76d142ea339eaebb
Steven-Ou <osteve425@gmail.com>
2025-08-18 13:14:05 -0400

    adding in the fa user

--- honey-academy/sanity/schemaTypes/aboutSection.ts
+++ honey-academy/sanity/schemaTypes/aboutSection.ts
@@ -47,7 +47,7 @@ export const aboutSection = defineType({
                   //People & Users
                   { title: "Teacher", value: "fas fa-chalkboard-teacher" },
                   { title: "Student", value: "fas fa-user-graduate" },
-                  
+                  { title: "Users", value: "fas fa-user" },
                   { title: "Happy Face", value: "fas fa-smile" },
                   { title: "Building/Campus", value: "fas fa-building" },
                 ],
