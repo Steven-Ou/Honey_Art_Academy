0367ef769ec44bc95f659992bdde56fc6d581e0f
Steven-Ou <osteve425@gmail.com>
2025-08-25 14:49:36 -0400

    adding in the decoration

--- honey-academy/components/ThemeToggleButton.js
+++ honey-academy/components/ThemeToggleButton.js
@@ -8,5 +8,10 @@ import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 export default function ThemeToggleButton() {
   const { theme, toggleTheme } = useTheme();
 
-  return <button onClick={toggleTheme} className="p"></button>;
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-full bg-gray-200:bg-gray-700 text-gray-800 dark:text-gray-200"
+    ></button>
+  );
 }
