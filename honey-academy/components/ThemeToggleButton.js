6a9ee325882473d17eec225c780afd8dc109f844
Steven-Ou <osteve425@gmail.com>
2025-08-25 14:47:23 -0400

    creating the function ThemeToggle Button

--- honey-academy/components/ThemeToggleButton.js
+++ honey-academy/components/ThemeToggleButton.js
@@ -4,3 +4,7 @@ import React from "react";
 import { useTheme } from "./ThemeProvider";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun,faMoon } from "@fortawesome/free-solid-svg-icons";
+
+export default function ThemeToggleButton(){
+    
+}
