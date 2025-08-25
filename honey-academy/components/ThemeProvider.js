a4ef5be8a916f9c092b827126e4b239e2c9c7bdc
Steven-Ou <osteve425@gmail.com>
2025-08-25 15:17:43 -0400

    adding in theme , setTheme

--- honey-academy/components/ThemeProvider.js
+++ honey-academy/components/ThemeProvider.js
@@ -5,5 +5,5 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-    
+    const [theme,setTheme] = useState('light');
 }
