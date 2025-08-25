11880e95031c54363c8ff235ffb9c54823b9eeb9
Steven-Ou <osteve425@gmail.com>
2025-08-25 14:46:52 -0400

    adding in imports

---
+++ honey-academy/components/ThemeToggleButton.js
@@ -0,0 +1,6 @@
+"use client";
+
+import React from "react";
+import { useTheme } from "./ThemeProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun,faMoon } from "@fortawesome/free-solid-svg-icons";
