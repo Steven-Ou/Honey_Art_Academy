a36871a95a53155431d1b4d5e527627da11e1053
Steven-Ou <osteve425@gmail.com>
2025-08-25 15:04:16 -0400

    adding in import

--- honey-academy/components/Header.js
+++ honey-academy/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-
+import ThemeToggleButton from "./ThemeToggleButton";
 // This is no longer an async component
 export default function Header({ data }) {
   const { menuItems } = data || {};
