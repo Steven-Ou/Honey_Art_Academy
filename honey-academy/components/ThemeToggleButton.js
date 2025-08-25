09bb47e3cd3b76e28ab86a5932439a101c062c2d
Steven-Ou <osteve425@gmail.com>
2025-08-25 15:27:14 -0400

    adding in fix

--- honey-academy/components/ThemeToggleButton.js
+++ honey-academy/components/ThemeToggleButton.js
@@ -11,7 +11,7 @@ export default function ThemeToggleButton() {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 rounded-full bg-gray-200:bg-gray-700 text-gray-800 dark:text-gray-200"
+      className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200"
       aria-label="Toggle theme"
     >
       {theme === "light" ? (
