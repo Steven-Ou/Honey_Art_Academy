40c20eac5bb35dd1870b9b681b66efa068927e74
Steven-Ou <osteve425@gmail.com>
2025-08-25 14:50:06 -0400

    adding an aria-label

--- honey-academy/components/ThemeToggleButton.js
+++ honey-academy/components/ThemeToggleButton.js
@@ -12,6 +12,7 @@ export default function ThemeToggleButton() {
     <button
       onClick={toggleTheme}
       className="p-2 rounded-full bg-gray-200:bg-gray-700 text-gray-800 dark:text-gray-200"
+      aria-label="Toggle theme"
     ></button>
   );
 }
