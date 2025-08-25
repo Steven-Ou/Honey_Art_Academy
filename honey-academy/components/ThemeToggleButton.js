20f81833f25aceb49c8da0881016f7e2ff91f66c
Steven-Ou <osteve425@gmail.com>
2025-08-25 14:47:59 -0400

    adding in the theme and toggletheme

--- honey-academy/components/ThemeToggleButton.js
+++ honey-academy/components/ThemeToggleButton.js
@@ -3,8 +3,9 @@
 import React from "react";
 import { useTheme } from "./ThemeProvider";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun,faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
-export default function ThemeToggleButton(){
-    
+export default function ThemeToggleButton() {
+  const { theme, toggleTheme } = useTheme();
+  
 }
