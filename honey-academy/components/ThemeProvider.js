3e00e9e1b2c365246e82d7fff2da3dcd93572bc2
Steven-Ou <osteve425@gmail.com>
2025-08-25 15:19:57 -0400

    adding in document to remove the light and also fixing the structure

--- honey-academy/components/ThemeProvider.js
+++ honey-academy/components/ThemeProvider.js
@@ -5,14 +5,14 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-    const [theme,setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
-    useEffect(()=>{
-        const storedTheme = localStorage.getItem('theme') || 'light';
-        setTheme(storedTheme);
-    },[]);
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme") || "light";
+    setTheme(storedTheme);
+  }, []);
 
-    useEffect(()=>{
-        
-    })
+  useEffect(() => {
+    document.documentElement.classList.remove("light", "dark");
+  });
 }
