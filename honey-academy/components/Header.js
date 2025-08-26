f30afdcf9e30e7ddb80fdc0f7149d742be78fe97
Steven-Ou <osteve425@gmail.com>
2025-08-25 20:32:54 -0400

    Applying the dark mode to the header and footer

--- honey-academy/components/Header.js
+++ honey-academy/components/Header.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Link from "next/link";
 import ThemeToggleButton from "./ThemeToggleButton";
-// This is no longer an async component
+
 export default function Header({ data }) {
   const { menuItems } = data || {};
 
   return (
-    <header className="bg-white shadow-md sticky top-0 z-50">
+    <header className="bg-white dark:bg-dark-surface shadow-md sticky top-0 z-50">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold text-primary">
+        <div className="text-2xl font-bold text-primary dark:text-dark-primary">
           <Link href="/">Honey Art Academy</Link>
         </div>
         <nav className="hidden md:flex items-center space-x-8">
           {menuItems.map((item) => (
             <Link key={item._key} href={item.url}>
-              <div className="text-gray-600 hover:text-primary transition-colors duration-300">
+              <div className="text-gray-600 dark:text-dark-text_light hover:text-primary transition-colors duration-300">
                 {item.label}
               </div>
             </Link>
@@ -24,10 +24,10 @@ export default function Header({ data }) {
               Contact Us
             </div>
           </Link>
-          <ThemeToggleButton/>
+          <ThemeToggleButton />
         </nav>
         <div className="md:hidden">
-          <button className="text-gray-600 hover:text-primary">
+          <button className="text-gray-600 dark:text-dark-text_light hover:text-primary">
             <i className="fas fa-bars text-2xl"></i>
           </button>
         </div>
