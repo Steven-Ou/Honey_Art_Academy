7cf046fc4b6f54eeea1547cf0271e78774e2a8d1
Steven-Ou <osteve425@gmail.com>
2025-08-26 00:38:48 -0400

    useEffect function

--- honey-academy/hooks/useScrollAnimation.js
+++ honey-academy/hooks/useScrollAnimation.js
@@ -5,4 +5,8 @@ import { useEffect, useRef, useState } from "react";
 export const useScrollAnimation = (options) => {
   const ref = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(()=>{
+    
+  })
 };
