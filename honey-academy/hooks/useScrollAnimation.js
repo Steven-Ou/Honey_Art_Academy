55af9b0fff5a14d09c1a84ef05f66ac1527a8454
Steven-Ou <osteve425@gmail.com>
2025-08-26 00:38:26 -0400

    adding isVisible and setIsVisible

--- honey-academy/hooks/useScrollAnimation.js
+++ honey-academy/hooks/useScrollAnimation.js
@@ -4,4 +4,5 @@ import { useEffect, useRef, useState } from "react";
 
 export const useScrollAnimation = (options) => {
   const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 };
